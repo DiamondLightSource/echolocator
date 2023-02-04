c61c59a60d4a17b3e8cdc8a47e3d2a03c5432356
David Erb <david.erb@diamond.ac.uk>
2023-02-04 05:50:24 +0000

    gets code from old repo

---
+++ .dae-devops/prepare_git_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ********** Please don't edit this file!
+# ********** It has been generated automatically by dae_devops version 0.4.1.dev0+g5d50877.d20230202.
+# ********** For repository_name echolocator
+
+me=${BASH_SOURCE}
+echo "${me}: installing 0 dependencies for repository_name echolocator"
+
+function __install {
+    echo ${me}: "$@"
+    "$@"
+}
+
+
+# dae_devops_fingerprint 8024a3374581bfef1e1262baaca81f98
